685958ae0c273c13d7b42b10a5ffbb899c80ecea
Ishwar Rimal <ishwarrimal@sysadms-MacBook-Pro.local>
2019-12-05 08:17:27 +0530

    Impelemtnting useContext and  useEffect with class component

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+This project is for anyone who is new to electron and/or react. (But not an absolute beginner. If you have basic kowledge of `state`, `props` etc you may proceed further)
+Most of the concepts are of react, so you can use this as a base to learn react as well even without electron.
+Since this is done for electron, might not work in browser straightforward.. things like `ipcRenderer` `web-security` have been set particularly for 
+electron app.
+
+
 Project contains code for implementation of react with electron. 
 
 Steps to run:
@@ -10,20 +16,28 @@ Steps to run:
 
 There are multiple commits, each with different concepts:
 
-1. commit : 9ee3ce9bd3206c5f360118c7bd259b299bfc1aad
-contains implementation of redux with saga
+1. contains implementation of redux with saga  
+   commit : `9ee3ce9bd3206c5f360118c7bd259b299bfc1aad`
 
-2. commit: 3dd963e8aaee4d99a2305c314fc42b3d1f26288c
-contains code for setting up bitbucket pipeline
 
-3. commit: e6a42dd3f934c82983cfab7874c1a8f998ee65bf
-setting up error logger in the main process
+2. contains code for setting up bitbucket pipeline  
+    commit: `3dd963e8aaee4d99a2305c314fc42b3d1f26288c`
 
-4. commit: 3b8c7e060334eb25b0291e9adc1a1bb269ee5c95
-Creted error boundary component which console logs any unhandled exceptions
+3. setting up error logger in the main process  
+    commit: `e6a42dd3f934c82983cfab7874c1a8f998ee65bf`
 
-5. commit: da9f3c745ea2b7aed59f41708ef98279430e9db0
-IPC between main and renderer process for logging errors to file
 
-6. commit: dd9aa162b3c1b6450d753e1e78d125f8c70b5e6c
-Implementing hooks (useState, useEffect and useContext)
+4. Creted error boundary component which console logs any unhandled exceptions  
+    commit: `3b8c7e060334eb25b0291e9adc1a1bb269ee5c95`
+
+
+5. IPC between main and renderer process for logging errors to file  
+    commit: `da9f3c745ea2b7aed59f41708ef98279430e9db0`
+
+
+6. Implementing hooks (useState, useEffect and useContext)  
+    commit: `dd9aa162b3c1b6450d753e1e78d125f8c70b5e6c`
+
+
+7. Implementing useContext and useEffect with class component  
+    commit : `d16356bc4e6a308cd00d8787722d90d216988f04`
--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Button from './components/Button';
@@ -7,27 +7,51 @@ import ErrorBoundary from './components/ErrorBoundary';
 import { LoginContext } from './context';
 import { Login } from './components/Login';
 
-function App() {
-  const [isAuth, updateIsAuth] = useState(false) //local state to this component
-  const loginInfo = useState({isAuthenticated: false, userName: 'The Humble being'}) // creating state to be passed to the context
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {isAuth : false, loginInfo : {isAuthenticated: false, userName: 'The Humble Being'} }
+  }
+
+  componentDidUpdate(prevProps,prevState){
+    this.setState({isAuth: this.state.loginInfo.isAuthenticated === true})
+  }
+
+  shouldComponentUpdate(nextProps, nextState){
+    return this.state.loginInfo.isAuthenticated !== nextState.loginInfo.isAuthenticated || 
+            this.state.isAuth !== nextState.isAuth
+  }
+  updateIsAuth = value => {
+    this.setState({isAuth : value})
+  }
+
+  updateLogoinInfo = value => {
+    this.setState({loginInfo : value})
+
+  }
+
+  // const [isAuth, updateIsAuth] = useState(false) //local state to this component
+  // const loginInfo = useState({isAuthenticated: false, userName: 'The Humble being'}) // creating state to be passed to the context
   // Whenever context changes, we use useEffect to chagne our local state value
-  useEffect(() => {
-    updateIsAuth(loginInfo[0].isAuthenticated)
-  }, [loginInfo]);
-  return (
-    <ErrorBoundary>
-      <LoginContext.Provider value={loginInfo} >
-        <div className="App">
-          <Login />
-          <h1 >
-            Helloo
-          </h1>
-          <Button />
-          {isAuth ? <List /> : ''}
-        </div>
-      </LoginContext.Provider>
-    </ErrorBoundary>
-  );
+  // useEffect(() => {
+  //   updateIsAuth(loginInfo[0].isAuthenticated)
+  // }, [loginInfo]);
+  render(){
+    return (
+      <ErrorBoundary>
+        <LoginContext.Provider value={[this.state.loginInfo, this.updateLogoinInfo]} >
+          <div className="App">
+            <Login />
+            <h1 >
+              Helloo
+            </h1>
+            <Button />
+            {this.state.isAuth ? <List /> : ''}
+          </div>
+        </LoginContext.Provider>
+      </ErrorBoundary>
+    );
+  }
 }
 
 export default App;
